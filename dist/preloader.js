b526651e280e96ae0747e5ad783df2280716a25f
yarkovaleksei <yarkov_aleksei@mail.ru>
2016-07-01 20:35:27 +0300

    Update to v1.0.2

--- dist/preloader.js
+++ dist/preloader.js
@@ -1,10 +1,21 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-	(function(){
+var preloaderCSS3 = function(message){
+	message = ('string' === typeof message && message.length > 0) ? '<h1 class="css3">'+message+'</h1>' : '';
+	var container = document.createElement('div');
+	container.className = "css3 container";
+	container.innerHTML = '<div class="css3 wrapper">\
+						<div class="css3 part left"></div>\
+						'+message+'\
+						<div class="css3 round1 round2"></div>\
+						<div class="css3 part right"></div>\
+					</div>';
+	document.body.appendChild(container);
+
+	document.addEventListener("DOMContentLoaded", function(event) {
 		setTimeout(function(){
 			var round1 = document.querySelector('.css3.round1');
 			round1.className += ' hidden';
-			var message = document.querySelector('h1.css3');
-			message.className += ' hidden';
+			var h1 = document.querySelector('h1.css3');
+			h1.className += ' hidden';
 		}, 2000);
 		setTimeout(function(){
 			var left = document.querySelector('.css3.part.left');
@@ -16,5 +27,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			var preloader = document.querySelector('.css3.container');
 			preloader.className += ' fadeOut';
 		}, 3500);
-	})();
-});
+	});
+}
